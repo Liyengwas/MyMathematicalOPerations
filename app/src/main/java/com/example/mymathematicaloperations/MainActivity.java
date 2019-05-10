4c11c49cee661863a1454361668a0e4b7f8b463d
Liyengwas <liyengwas@gmail.com>
2019-05-10 09:29:47 +0300

    My Second commit with java included functionalities

--- app/src/main/java/com/example/mymathematicaloperations/MainActivity.java
+++ app/src/main/java/com/example/mymathematicaloperations/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     EditText input1, input2;
     String value1, value2;
     Double val1, val2, total;
-    TextView txtResult;
+    TextView textResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,31 +22,70 @@ public class MainActivity extends AppCompatActivity {
         //reference the components to the layout file
         input1 = findViewById(R.id.input1);
         input2 = findViewById(R.id.input2);
+        textResults = findViewById(R.id.textResults);
 
-        //Get the text entered in the editext and save it in the string
-        value1 = input1.getText().toString();
-        value2 = input2.getText().toString();
-
-//        //Convert the sting to double
-//        val1 = Double.parseDouble(value1);
-//        val2 = Double.parseDouble(value2);
 
     }
 
     public void addition(View view) {
+        value1 = input1.getText().toString();
+        value2 = input2.getText().toString();
+
+        //Convert the string to double
+        val1 = Double.parseDouble(value1);
+        val2 = Double.parseDouble(value2);
+
         //do the addition and display the results in the textview
         total = val1 + val2;
-        // txtResult.setText(total.toString());
+
+        // textResult.setText(total.toString());
         String finalresult = String.valueOf(total);
-        txtResult.setText(finalresult);
+        textResults.setText(finalresult);
     }
 
     public void subtraction(View view) {
+        value1 = input1.getText().toString();
+        value2 = input2.getText().toString();
+
+        //Convert the string to double
+        val1 = Double.parseDouble(value1);
+        val2 = Double.parseDouble(value2);
+
         //do the subtraction and display the results in the textview
         total = val1 - val2;
         //tvResult.setText(total.toString());
         String finalresult = String.valueOf(total);
-        txtResult.setText(finalresult);
+        textResults.setText(finalresult);
+    }
+
+    public void multiplication(View view){
+        value1 = input1.getText().toString();
+        value2 = input2.getText().toString();
+
+        //Convert the string to double
+        val1 = Double.parseDouble(value1);
+        val2 = Double.parseDouble(value2);
+
+        //do the multiplication and display the results in the textview
+        total = val1 * val2;
+        //tvResult.setText(total.toString());
+        String finalresult = String.valueOf(total);
+        textResults.setText(finalresult);
+    }
+
+    public void division(View view){
+        value1 = input1.getText().toString();
+        value2 = input2.getText().toString();
+
+        //Convert the string to double
+        val1 = Double.parseDouble(value1);
+        val2 = Double.parseDouble(value2);
+
+        //do the division and display the results in the textview
+        total = val1 / val2;
+        //tvResult.setText(total.toString());
+        String finalresult = String.valueOf(total);
+        textResults.setText(finalresult);
     }
 
 
